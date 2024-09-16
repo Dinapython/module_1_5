774a7e6e5f50a58e09f99ce175243afcb68502f9
Dinapython <gafurovamadi@gmail.com>
2024-09-16 14:58:49 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,6 @@
+immutable_var = 10, 0.5, 'Dina'
+print(immutable_var)
+#immutable_var [3] = 'Anya' #Кортеж хранит дату моего рождения и имя. Они не изменяемы.
+mutable_list = ['pear', 'ice crem', 5, 7, 7 ]
+mutable_list [3] = 6
+print(mutable_list)
